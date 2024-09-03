793ee3ce09200795accb82b33b498acb55f25c56
daifiyum <daiqisen131@gmail.com>
2024-09-03 12:00:11 +0800

    update ui

---
+++ frontend/src/components/Snackbar.vue
@@ -0,0 +1,26 @@
+<template>
+    <v-snackbar v-model="snackbarVisible" :timeout="2000" color="primary" variant="tonal">
+        {{ message }}
+        <template v-slot:actions>
+            <v-btn color="blue" variant="text" @click="snackbarVisible = false" icon="mdi-close-circle-outline">
+            </v-btn>
+        </template>
+    </v-snackbar>
+</template>
+
+<script setup>
+import { ref } from "vue"
+const snackbarVisible = ref(false)
+const message = ref("")
+
+function openSnackbar(msg) {
+    message.value = msg;
+    snackbarVisible.value = true;
+}
+
+defineExpose({
+    openSnackbar,
+});
+</script>
+
+<style lang="css" scoped></style>
--- frontend/src/views/Layout/components/LayoutDrawer.vue
+++ frontend/src/views/Layout/components/LayoutDrawer.vue
@@ -8,23 +8,29 @@
       <v-btn icon="mdi-close" @click="drawer = !drawer"></v-btn>
     </v-toolbar>
     <v-container>
-      <v-text-field
-        v-model="updateDelay"
-        label="更新延时"
-        clearable
-        @blur="setUpdateDelay"
-      ></v-text-field>
+      <v-text-field v-model="updateDelay" label="更新延时" clearable></v-text-field>
     </v-container>
+    <template v-slot:append>
+      <v-divider></v-divider>
+      <div class="pa-4 flex-column">
+        <v-btn color="primary" block prepend-icon="mdi-content-save-check-outline" variant="flat" @click="setUpdateDelay">
+          保存设置
+        </v-btn>
+      </div>
+    </template>
   </v-navigation-drawer>
+  <SnackBar ref="snackbarRef" />
 </template>
 
 <script setup>
 import { ref, onMounted } from "vue";
+import SnackBar from "../../../components/Snackbar.vue"
 import { getDelay, setDelay } from "@/api/config";
 import { storeToRefs } from "pinia";
 import { useDrawerStore } from "@/stores/drawer";
 const drawerStore = useDrawerStore();
 const { drawer } = storeToRefs(drawerStore);
+const snackbarRef = ref(null);
 
 // 设置抽屉
 const updateDelay = ref("");
@@ -32,8 +38,14 @@ onMounted(async () => {
   const { data } = await getDelay();
   updateDelay.value = data.update_delay;
 });
+
 async function setUpdateDelay() {
   await setDelay({ update_delay: updateDelay.value });
+  showSnackbar("新设置已保存")
+}
+
+function showSnackbar(msg) {
+  snackbarRef.value.openSnackbar(msg);
 }
 </script>
 
