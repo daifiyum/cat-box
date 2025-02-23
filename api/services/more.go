1b8b52a5e5dcd020dea98edba3e5f17a73914672
daifiyum <daiqisen131@gmail.com>
2025-02-23 19:40:02 +0800

    fix websocket

--- api/services/more.go
+++ api/services/more.go
@@ -11,10 +11,28 @@ func ShowBoxLogs(c *websocket.Conn) {
 		U.Broadcaster.Unsubscribe(logChan)
 		c.Close()
 	}()
-	for logLine := range logChan {
-		err := c.WriteMessage(websocket.TextMessage, []byte(logLine))
-		if err != nil {
+
+	done := make(chan struct{})
+	go func() {
+		defer close(done)
+		for {
+			if _, _, err := c.ReadMessage(); err != nil {
+				return
+			}
+		}
+	}()
+
+	for {
+		select {
+		case <-done:
 			return
+		case logLine, ok := <-logChan:
+			if !ok {
+				return
+			}
+			if err := c.WriteMessage(websocket.TextMessage, []byte(logLine)); err != nil {
+				return
+			}
 		}
 	}
 }
