665d7676e3b6532d0a3c8a096d29d3a017803a5c
daifiyum <daiqisen131@gmail.com>
2025-02-24 18:03:09 +0800

    fix

--- main.go
+++ main.go
@@ -5,11 +5,11 @@ import (
 
 	"github.com/daifiyum/cat-box/api"
 	"github.com/daifiyum/cat-box/app"
+	"github.com/daifiyum/cat-box/app/tray"
 	"github.com/daifiyum/cat-box/database"
 	I "github.com/daifiyum/cat-box/initializer"
 	S "github.com/daifiyum/cat-box/singbox"
 	"github.com/daifiyum/cat-box/tasks"
-	"github.com/daifiyum/cat-box/tray"
 )
 
 func init() {
